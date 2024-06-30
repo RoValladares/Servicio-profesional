77ba18390eaf0ac717fb7b31b44fa8b18b4c67f2
I am Rodry <rodrigo_valladares94@hotmail.com>
2024-06-29 20:50:52 -0600

    un arreglo mas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    document.getElementById('confirmationMessage').textContent = `¡Gracias, ${name}! Hemos recibido tu mensaje: "${message}". Wil y Ardy se pondrán en contacto contigo en ${email} pronto.`;
+    document.getElementById('confirmationMessage').textContent = `¡Gracias, ${name}! Hemos recibido tu mensaje: "${message}". Wil se pondra en contacto contigo en ${email} pronto.`;
     document.getElementById('confirmationMessage').classList.remove('hidden');
 
     // Clear the form
